beea987a5070402eeb141c331e6deb4d03af1ac7
kasoqian <95743792+kasoqian@users.noreply.github.com>
2022-06-13 14:48:35 +0800

    feat: 修改样式

--- src/app/src/App.vue
+++ src/app/src/App.vue
@@ -112,6 +112,7 @@
           alt="CheersPal logo"
         />
         <h1 class="heading">Cheers pals</h1>
+        <!-- TODO -->
         <a
           data-w-id="57408da1-b59b-fad3-0308-861394b4b6b8"
           style="
@@ -127,6 +128,24 @@
           href="#"
           class="button w-button"
           >MINT</a
+        >
+        <!-- TODO -->
+        <a
+          data-w-id="57408da1-b59b-fad3-0308-861394b4b6b8"
+          style="
+            -webkit-transform: translate3d(0, 0, 0) scale3d(0, 0, 1) rotateX(0)
+              rotateY(0) rotateZ(0) skew(0, 0);
+            -moz-transform: translate3d(0, 0, 0) scale3d(0, 0, 1) rotateX(0)
+              rotateY(0) rotateZ(0) skew(0, 0);
+            -ms-transform: translate3d(0, 0, 0) scale3d(0, 0, 1) rotateX(0)
+              rotateY(0) rotateZ(0) skew(0, 0);
+            transform: translate3d(0, 0, 0) scale3d(0, 0, 1) rotateX(0)
+              rotateY(0) rotateZ(0) skew(0, 0);
+            background-color: #fc6363;
+          "
+          href="#"
+          class="button w-button"
+          >WhiteLists MINT</a
         >
       </div>
     </div>
